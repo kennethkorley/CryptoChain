49a9e5731a18c53eec8fc53915648806a7003ecc
Adil Ahmad <kennethkorley0410@ccdot.email>
2026-07-16 22:57:26 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/cryptochainx/main.go
@@ -0,0 +1,23 @@
+// cmd/cryptochainx/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cryptochainx/internal/cryptochainx"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cryptochainx.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
